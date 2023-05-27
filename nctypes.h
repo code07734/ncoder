e7d55f0642f0deaae917f1c773500fd5db1da817
Vladimir <47279914+code07734@users.noreply.github.com>
2023-05-27 23:43:10 +0700

    Create nctypes.h

---
+++ nctypes.h
@@ -0,0 +1,9 @@
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+typedef long long i64;
+typedef int i32;
+typedef short int i16;
+typedef char i8;
+typedef unsigned long msu32;
